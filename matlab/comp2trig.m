43d1722fe9450fcdab16e79d38ca3c88884da534
yuval-harpaz <yuvharpaz@gmail.com>
2011-01-06 08:48:00 +0200

    updating epilepsy procedures

--- matlab/comp2trig.m
+++ matlab/comp2trig.m
@@ -9,40 +9,24 @@ trigger=zeros(1,size(comp.trial{1,1}(1,:),2));
 for c=compNum
     [posPeak,negPeak]=peakDetection(comp.trial{1,1}(c,:),3);
     title(num2str(c))
-    i=find(posPeak>0);
+    p=find(posPeak>0);
     n=find(negPeak<0);
-    if sum(n>0)>sum(i>0)
+    i=p;
+    trig=posPeak*c;
+    if sum(n>0)>sum(p>0)
         i=n;
+        trig=negPeak*c;
     end
-    %display(['found ',num2str(sum(i)),' spikes']);
     dif=diff(i);
     difi=find(dif<200);
-    %display([num2str(size(difi,2)),' spikes were rejected , they were less than 200 samples after another spike'])
-    trig=posPeak*c;
     for j=1:size(difi,2)  % deleting spikes 200 samples after other spikes
-        trig(1,i(1,(j+1)))=0;
+        trig(1,i(1,(difi(j)+1)))=0;
     end
     trig(1,(size(trig,2)+1))=0;
     trigger=trigger+trig;
+    hold on; plot(trig,'g')
+    title([num2str(size(find(trig),2)),' spikes remained (green)']);
+    display([num2str(size(find(trig),2)),' spikes remained (green)']);
 end
-% save([pat,'trigger'],'trigger')
-% cd(pat);
-% rewriteTrig(dataset,trigger,[]);
+
 end
-
-% [posPeak,negPeak]=YHpeakDetection(comp_orig.trial{1,1}(compNum,:),3);
-% i=find(posPeak>0);
-% display(['found ',num2str(sum(i)),'positive peaks']);
-% dif=diff(i);
-% difi=find(dif<200);
-% display([num2str(size(difi,2)),' spikes were rejected , they were less than 200 samples after another spike'])
-% trig=posPeak*10;
-% for j=1:size(difi,2)  % deleting spikes 200 samples after other spikes
-%     trig(1,i(1,(j+1)))=0;
-% end
-% trig(1,(size(trig,2)+1))=0;
-% save([pat,'trig'],'trig')
-% cd(pat);
-% rewriteTrig(dataset,trig,[]);
-
-
--- matlab/prepEpi4erf.m
+++ matlab/prepEpi4erf.m
@@ -9,13 +9,15 @@ comp=epiFTica(pat,dataset,[74 204],'M');
 %% 2) create a trigger channel
 % check the components with component browser.
 % specify components 'compNum' to be written as triggers. try compNum=1:10 for start.
-compNum=4;
+compNum=3;
 trigger=comp2trig(comp,compNum);
 %% 3) write the new trigger channel to file
 % has to write one trigger value per file because SAM crashes when triggers
 % overlap
 % specify bad channels to be replaced by zeros.
-cd(pat);
+if ~isempty(pat)
+    cd(pat);
+end
 for i=1:size(compNum,2)
     trig=compNum(i)*(trigger==compNum(i));
     trig(1,1:200)=0;trig(1,(end-200):end)=0; %ignoring edges
@@ -23,6 +25,9 @@ for i=1:size(compNum,2)
 end
 %%
 % in a terminal run
-% SAMerf -r b024 -d tf4_c,rfhp1.0Hz,ee -w Global,0-100Hz,Global,ECD -m 4 -f "3 70" -v -t "-0.05 0.05" -b "-0.15 -0.05" -z 3
+% SAMwts -r b099 -d tf3_c,rfhp1.0Hz,ee -c Global,20-70Hz -C -Z -x "-10 10" -y "-9 9" -z "0 14" -s 0.5 -v
+% SAMspm -r b099 -d tf3_c,rfhp1.0Hz,ee -a Global,0-100Hz,Global,ECD -c Global,0-100Hz,Global,ECD -m ica3 -D 1 -P -v
+% SAMerf -r b099 -d tf3_c,rfhp1.0Hz,ee -w Global,0-100Hz,Global,ECD -m 3 -f "3 70" -v -t "-0.05 0.05" -b "-0.15 -0.05" -z 3
+
     
     

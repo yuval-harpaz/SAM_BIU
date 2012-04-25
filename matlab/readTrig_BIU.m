031de7e6b29bcf156f4008e5c795b01efb58d9b5
yuval <yuval@bar-ilan>
2012-04-25 11:57:56 +0300

    a couple of little things

--- matlab/readTrig_BIU.m
+++ matlab/readTrig_BIU.m
@@ -1,5 +1,12 @@
 function trig=readTrig_BIU(source);
 %% reads the trigger channel
+if ~exist('source','var')
+    source=[];
+end
+if isempty(source)
+    source='c,rfhp0.1Hz';
+    display(['looking for default source ',source])
+end
 pdf=pdf4D(source);
 hdr=get(pdf,'header');
 lastSamp=hdr.epoch_data{1,1}.pts_in_epoch;

6ddace76ad5091f606045b9600d18f5d365f6f91
yuval <yuval@yuval-HP>
2012-05-07 06:20:34 +0300

    move sam weights to ft format

---
+++ matlab/wts2filter.m
@@ -0,0 +1,18 @@
+function filter=wts2filter(ActWgts,inside,outN)
+% inside=sourceGlobal.inside;
+% outN=size(sourceGlobal.outside,1);
+
+sensN=248;
+srcN 	   = size(ActWgts,1);
+for i=1:(srcN+outN)
+    filter{i}=[];
+end
+for i=1:srcN,
+    si = inside(i);
+    %if ~isempty(sourceGlobal.avg.filter{m})
+        %                sourceAvg.mom{m} = sourceGlobal.avg.filter{m}(1,ismeg)*EMSEdata;
+    %    sourceAvg.mom{m} = sourceGlobal.avg.filter{m}*standard.avg;
+    filter{si}=ActWgts(i,:);
+        
+    %end
+end

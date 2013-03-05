847c3d5f36cece5ba337ed8d5c4746d656281a91
yuval <yuval@bar-ilan>
2013-03-05 13:25:28 +0200

    getting rid of 1024

--- matlab/clearTrig.m
+++ matlab/clearTrig.m
@@ -3,7 +3,7 @@ warning('50Hz cleaning with cleanMEG pack will not be possible using the new tri
 trig16=uint16(trig);
 trigf=bitset(trig16,9,0); %getting rid of trigger 256 (9)
 trigf=bitset(trigf,10,0);  %getting rid of trigger 512 (10)
-%trigf=bitset(trigf,11,0);  %getting rid of trigger 1024 (11)
+trigf=bitset(trigf,11,0);  %getting rid of trigger 1024 (11)
 newtrig=single(trigf);
 newtrig(newtrig>256)=0;
 newtrigonset=newtrig(1,:);newtrigonset(1,1)=0;newtrigonset(1,2:end)=newtrigonset(1,2:end)-newtrigonset(1,1:(end-1));

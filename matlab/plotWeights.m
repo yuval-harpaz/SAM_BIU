f253641211e9fce2b3a7bef261aa55425e5edc4a
yuval <yuval@bar-ilan>
2013-04-24 12:00:18 +0300

    allow plots for list of wts

--- matlab/plotWeights.m
+++ matlab/plotWeights.m
@@ -1,6 +1,8 @@
 function plotWeights(wtsFile,vox);
 %% reading weights
-
+% if you use a grid with fixed distances (SAM default) vox is [x y z];
+% if you gave SAM a text file with n voxels then vox is an index of the
+% virtual sensor [i] or column of virt. sensors [100;2342;232]
 if strcmp('.wts',wtsFile(end-3:end)) || strcmp('.mat',wtsFile(end-3:end))
     wtsNoSuf=wtsFile(1:end-4);
 else
@@ -33,18 +35,22 @@ if ischar(vox) % checks if vox is a text file
 end
 load ~/ft_BIU/matlab/plotwts
 for voxi=1:size(vox,1)
-        if SAMHeader.StepSize==0; % which means it is not a box of voxels
-            wts.avg=ActWgts(voxi,:)';
-            figure;ft_topoplotER([],wts);
-            title(num2str(vox(voxi,:)));
-        else
-            ind=voxIndex(vox(voxi,:),100.*[...
-                SAMHeader.XStart SAMHeader.XEnd ...
-                SAMHeader.YStart SAMHeader.YEnd ...
-                SAMHeader.ZStart SAMHeader.ZEnd],...
-                100.*SAMHeader.StepSize,0);
-            wts.avg=ActWgts(ind,:)';
-            figure;ft_topoplotER([],wts);
-            title(num2str(vox(voxi,:)));
+    if SAMHeader.StepSize==0; % which means it is not a box of voxels
+        if size(vox,2) > 1
+            error('no box, please give vox as an index (say vox=3 or [3;7;12]);');
         end
+        
+        wts.avg=ActWgts(vox(voxi),:)';
+        figure;ft_topoplotER([],wts);
+        title(num2str(vox(voxi,:)));
+    else
+        ind=voxIndex(vox(voxi,:),100.*[...
+            SAMHeader.XStart SAMHeader.XEnd ...
+            SAMHeader.YStart SAMHeader.YEnd ...
+            SAMHeader.ZStart SAMHeader.ZEnd],...
+            100.*SAMHeader.StepSize,0);
+        wts.avg=ActWgts(ind,:)';
+        figure;ft_topoplotER([],wts);
+        title(num2str(vox(voxi,:)));
     end
+end

890f61a6e495461ad8fd091082e33e7138cc45de
yuval-harpaz <yuvharpaz@gmail.com>
2015-11-23 15:50:24 +0200

    gitUpdate

--- matlab/clearTrig.m
+++ matlab/clearTrig.m
@@ -1,9 +1,14 @@
-function [newtrig]=clearTrig(trig)
-%warning('50Hz cleaning with cleanMEG pack will not be possible using the new trigger'); %#ok<WNTAG>
-trig16=uint16(trig);
-trigf=bitset(trig16,9,0); %getting rid of trigger 256 (9)
-trigf=bitset(trigf,10,0);  %getting rid of trigger 512 (10)
-trigf=bitset(trigf,11,0);  %getting rid of trigger 1024 (11)
+function [newtrig]=clearTrig(trig,values)
+if ~exist('values','var')
+    values=[256,512,1024];
+end
+bits=log2(values)+1;
+trigf=uint16(trig);
+for biti=1:length(bits)
+    trigf=bitset(trigf,bits(biti),0); 
+end
+% trigf=bitset(trigf,10,0);  %getting rid of trigger 512 (10)
+% trigf=bitset(trigf,11,0);  %getting rid of trigger 1024 (11)
 newtrig=single(trigf);
 newtrig(newtrig>256)=0;
 newtrigonset=newtrig(1,:);newtrigonset(1,1)=0;newtrigonset(1,2:end)=newtrigonset(1,2:end)-newtrigonset(1,1:(end-1));
